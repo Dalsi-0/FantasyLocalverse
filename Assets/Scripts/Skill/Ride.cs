e2f2983514b925cfd66d59712c88c141233f6167
dalsi <tkdwns8632@gmail.com>
2025-02-20 09:59:18 +0900

    [ Feat ] 탑승 및 탑승 해제시 파티클 추가
    // 탑승 및 탑승 해제시 파티클 추가

--- Assets/Scripts/Skill/Ride.cs
+++ Assets/Scripts/Skill/Ride.cs
@@ -6,14 +6,27 @@ using UnityEngine.UI;
 
 public class Ride : SkillBase
 {
+    ParticleSystem myParticleSystem;
+
     public Ride(SkillDataSO data, Image image) : base(data, image)
     {
+        if(skillData.effectPrefab != null)
+        {
+            myEffectObject = SkillManager.Instance.CreateSkillEffectPrefabs(skillData.effectPrefab, GameManager.Instance.player.transform);
+            myParticleSystem = myEffectObject.transform.GetComponent<ParticleSystem>();
+        }
+        else
+        {
+            Debug.Log($"{data.name}¿« ¿Ã∆Â∆Æ «¡∏Æ∆’¿Ã null");
+        }
     }
     
     protected override void ExecuteSkill()
     {
         PlayerController player = GameManager.Instance.PlayerController;
-        
+
+        if (myParticleSystem != null) { myParticleSystem.Play(); }
+
         if (player != null)
         {
             player.ChangeSystemState(player.GetPlayerState() == EPlayerState.Normal 
